eb8c726d42d2122f20699730c8e0877b687c1e67
gonzalopozo <gonzalopozo2005@gmail.com>
2023-09-05 17:59:40 +0200

    ES6: object literals

---
+++ src/es6/05-object-literals.js
@@ -0,0 +1,12 @@
+// Enached object literals
+
+function newUser(user, age, country, uId) {
+    return {
+        user,
+        age,
+        country,
+        uId
+    }
+}
+
+console.log(newUser('gndx', 34, "MX", 1));
