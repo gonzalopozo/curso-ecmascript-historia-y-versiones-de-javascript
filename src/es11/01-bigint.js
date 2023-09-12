ad39844941733265e5e75597531f1f9cd1b1826e
gonzalopozo <gonzalopozo2005@gmail.com>
2023-09-12 12:33:08 +0200

    ES11: BigInt y Nullish

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 8546834679376904835680436973547036n;
+const anotherBigNumber = BigInt(8546834679376904835680436973547036);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
